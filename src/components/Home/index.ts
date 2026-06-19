6e6ecf7447e8b2305f65014d0c9feb43ba761cac
Tshepo Cleopatra Baartman <141533140+Ogleloo@users.noreply.github.com>
2026-06-19 11:00:01 +0200

    Update index.ts

--- src/components/Home/index.ts
+++ src/components/Home/index.ts
@@ -1,3 +1,5 @@
 export { default as StorySection } from './StorySection';
 export { default as CateringEventsSection } from './CateringEventsSection';
 export { default as FindUsSection } from './FindUsSection';
+export { default as CateringForm } from './CateringForm';
+export { default as EventsForm } from './EventsForm';
